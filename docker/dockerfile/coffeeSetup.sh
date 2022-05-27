72253d0fabfd0925b34d6c31547dd0d3226ee26a
Sergei <cyfqgthjr2020@yandex.ru>
2022-05-27 17:50:15 +0300

    fixed script

--- docker/dockerfile/coffeeSetup.sh
+++ docker/dockerfile/coffeeSetup.sh
@@ -1,1 +1,1 @@
-service postgresql start && coffee_machine /coffee_DB.backup && sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'cyfqgthjr202020';" && java -jar coffee.jar
+service postgresql start && sudo -u postgres createdb coffee_machine && sudo -u postgres pg_restore -d coffee_machine /coffee_DB.backup && sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'cyfqgthjr202020';" && java -jar coffee.jar
