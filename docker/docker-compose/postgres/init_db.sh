79196d42d58a1e3cca71ee3b59414a0965bc8674
Sergei <cyfqgthjr2020@yandex.ru>
2022-06-06 23:59:32 +0300

    Made init-script for database

---
+++ docker/docker-compose/postgres/init_db.sh
@@ -0,0 +1,5 @@
+if [ $( psql -U postgres -lqt | cut -d \| -f 1 | grep -w coffee_machine | wc -l ) = '0' ]; then
+  createdb coffee_machine && pg_restore -d coffee_machine /coffee_DB.backup && echo "Database created"
+else
+  echo "Database exists"
+fi
