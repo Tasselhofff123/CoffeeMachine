dc377e447ce7f95173f91477356fe872f21d6863
Sergei <cyfqgthjr2020@yandex.ru>
2022-06-13 18:45:13 +0300

    Added autoindex for logs to nginx

--- docker/docker-compose/postgres/init_db.sh
+++ docker/docker-compose/postgres/init_db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ $( psql -U postgres -lqt | cut -d \| -f 1 | grep -w coffee_machine | wc -l ) = '0' ]; then
   createdb coffee_machine && pg_restore -d coffee_machine /coffee_DB.backup && echo "Database created"
 else
